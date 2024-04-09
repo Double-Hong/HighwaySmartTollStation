8df55ab0e7bf8a84899cdf2304e01e14abe8ec5c
DoubleHong <greathongtian@gmail.com>
2024-04-09 23:23:29 +0800

    First Commit

---
+++ src/main/java/com/example/highwaysmarttollstation/HighwaySmartTollStationApplication.java
@@ -0,0 +1,13 @@
+package com.example.highwaysmarttollstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighwaySmartTollStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighwaySmartTollStationApplication.class, args);
+    }
+
+}
